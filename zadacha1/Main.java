6629c02d72f5a8e25fa08a0f6c03bf528c989f01
Alexander <1987kerykery@mail.ru>
2023-09-17 16:11:31 +1100

    oop_hw5

--- zadacha1/Main.java
+++ zadacha1/Main.java
@@ -2,6 +2,7 @@ package zadacha1;
 
 public class Main {
     public static void main(String[] args) {
+        // Создание экземпляров модели, представления и презентера
         CalculatorModel model = new CalculatorModel();
         CalculatorView view = new CalculatorView();
         CalculatorController calcController = new CalculatorController(model, view);
